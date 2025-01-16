29bcc325687f8928686d4c88bbf41f817c9ff688
Nathan Will Martins <nathanwillmartins@gmail.com>
2025-01-16 14:18:27 -0300

    feat: Inicio do Sistema Centralizado Multiprocesso

--- Trabalho2_Codigos_Imagens/main.py
+++ Trabalho2_Codigos_Imagens/main.py
@@ -4,6 +4,9 @@ from torchvision import datasets  # Importa os datasets da biblioteca torchvisio
 from torchvision.transforms import v2  # Importa as transformações de imagens da torchvision
 import time
 from itertools import product
+import os
+from multiprocessing import Pool, cpu_count
+
 
 # Define as transformações que serão aplicadas nas imagens de treino e teste
 def define_transforms(height, width):
@@ -30,42 +33,117 @@ def read_images(data_transforms):
     return train_data, validation_data, test_data  # Retorna os conjuntos de dados
 
 
-if __name__ == '__main__':
-    # Define as dimensões das imagens (224x224) e aplica as transformações
-    data_transforms = define_transforms(224, 224)
-    
-    # Carrega os dados de treino, validação e teste com as transformações aplicadas
-    train_data, validation_data, test_data = read_images(data_transforms)
-    
-    # Cria uma instância do modelo CNN com os dados carregados e o número de classes (8)
+def train_model_parallel(args):
+    model_name, num_epochs, learning_rate, weight_decay, replicacoes, train_data, validation_data, test_data = args
     cnn = CNN(train_data, validation_data, test_data, 8)
+    inicio = time.time()
+    acc_media, rep_max = cnn.create_and_train_cnn(model_name, num_epochs, learning_rate, weight_decay, replicacoes)
+    fim = time.time()
+    duracao = fim - inicio
+    return model_name, num_epochs, learning_rate, weight_decay, acc_media, rep_max, duracao
+
+
+if __name__ == '__main__':
+    inicio_total = time.time()  # Marca o início do programa
+
+    print("Escolha o sistema para execução:")
+    print("1. Centralizado e um único processo")
+    print("2. Centralizado e multiprocesso")
+    print("3. Distribuído e multiprocesso")
+    escolha = input("Digite o número correspondente ao sistema desejado: ")
+
+    if escolha == "1":
+        inicio_sistema = time.time()  # Início do sistema centralizado único
+        print("Sistema Centralizado em Único Processo Escolhido.")
+        # Define as dimensões das imagens (224x224) e aplica as transformações
+        data_transforms = define_transforms(224, 224)
+        
+        # Carrega os dados de treino, validação e teste com as transformações aplicadas
+        train_data, validation_data, test_data = read_images(data_transforms)
+        
+        # Cria uma instância do modelo CNN com os dados carregados e o número de classes (8)
+        cnn = CNN(train_data, validation_data, test_data, 8)
+        
+        # Configurações para treinamento do modelo
+        replicacoes = 2  # Número de repetições para treinar o modelo
+        model_names = ['alexnet', 'mobilenet_v3_large', 'mobilenet_v3_small', 'resnet18', 'resnet101', 'vgg11', 'vgg19']
+        epochs = [1]  # Número de épocas para treinamento
+        learning_rates = [0.001, 0.0001, 0.00001]  # Taxas de aprendizado
+        weight_decays = [0, 0.0001]  # Decaimento de peso
+
+        # Gera todas as combinações possíveis de parâmetros
+        parameter_combinations = product(model_names, epochs, learning_rates, weight_decays)
+
+        # Itera sobre cada combinação de parâmetros
+        for model_name, num_epochs, learning_rate, weight_decay in parameter_combinations:
+            inicio = time.time()  # Marca o início do tempo de treinamento
+
+            # Treina a CNN utilizando os parâmetros definidos
+            acc_media, rep_max = cnn.create_and_train_cnn(model_name, num_epochs, learning_rate, weight_decay, replicacoes)
+
+            fim = time.time()  # Marca o final do tempo de treinamento
+            duracao = fim - inicio  # Calcula a duração do treinamento
+
+            # Exibe os resultados do treinamento
+            print(f"Parâmetro de modelo: {model_name}")
+            print(f"Parâmetro de quantidade de épocas: {num_epochs}")
+            print(f"Parâmetro de Learning Rate: {learning_rate}")
+            print(f"Parâmetro de Weight Decay: {weight_decay}")
+            print(f"Acurácia Média: {acc_media}")
+            print(f"Melhor replicação: {rep_max}")
+            print(f"Tempo: {duracao:.2f} segundos\n")
+            print("-----------------------------------")
+        fim_sistema = time.time()
+        print(f"Tempo total para o sistema Centralizado Único Processo: {fim_sistema - inicio_sistema:.2f} segundos\n")
     
-    # Configurações para treinamento do modelo
-    replicacoes = 2  # Número de repetições para treinar o modelo
-    model_names = ['alexnet', 'mobilenet_v3_large', 'mobilenet_v3_small', 'resnet18', 'resnet101', 'vgg11', 'vgg19']
-    epochs = 1 # [10, 20]  # Número de épocas para treinamento
-    learning_rates = [0.001, 0.0001, 0.00001]  # Taxas de aprendizado
-    weight_decays = [0, 0.0001]  # Decaimento de peso
+    elif escolha == "2":
+        inicio_sistema = time.time()  # Início do sistema centralizado multiprocesso
+        print("Sistema Centralizado em Multiprocesso Escolhido.")
 
-    # Gera todas as combinações possíveis de parâmetros
-    parameter_combinations = product(model_names, epochs, learning_rates, weight_decays)
+        # Obter número de núcleos disponíveis
+        num_nucleos = cpu_count()
+        print(f"Usando {num_nucleos} núcleos para treinamento paralelo.")
 
-    # Itera sobre cada combinação de parâmetros
-    for model_name, num_epochs, learning_rate, weight_decay in parameter_combinations:
-        inicio = time.time()  # Marca o início do tempo de treinamento
+        # Define as dimensões das imagens (224x224) e aplica as transformações
+        data_transforms = define_transforms(224, 224)
+        train_data, validation_data, test_data = read_images(data_transforms)
 
-        # Treina a CNN utilizando os parâmetros definidos
-        acc_media, rep_max = cnn.create_and_train_cnn(model_name, num_epochs, learning_rate, weight_decay, replicacoes)
+        # Configurações para treinamento do modelo
+        replicacoes = 2
+        model_names = ['alexnet', 'mobilenet_v3_large', 'mobilenet_v3_small', 'resnet18', 'resnet101', 'vgg11', 'vgg19']
+        epochs = [1]
+        learning_rates = [0.001, 0.0001, 0.00001]
+        weight_decays = [0, 0.0001]
 
-        fim = time.time()  # Marca o final do tempo de treinamento
-        duracao = fim - inicio  # Calcula a duração do treinamento
+        parameter_combinations = list(product(model_names, epochs, learning_rates, weight_decays, [replicacoes]))
+        args = [(model_name, num_epochs, learning_rate, weight_decay, replicacoes, train_data, validation_data, test_data)
+                for model_name, num_epochs, learning_rate, weight_decay, replicacoes in parameter_combinations]
 
-        # Exibe os resultados do treinamento
-        print(f"\nParâmetro de modelo: {model_name}")
-        print(f"Parâmetro de quantidade de épocas: {num_epochs}")
-        print(f"Parâmetro de Learning Rate: {learning_rate}")
-        print(f"Parâmetro de Weight Decay: {weight_decay}")
-        print(f"Acurácia Média: {acc_media}")
-        print(f"Melhor replicação: {rep_max}")
-        print(f"Tempo: {duracao:.2f} segundos\n")
-        print("-----------------------------------")
+        # Multiprocessamento
+        with Pool(processes=num_nucleos) as pool:
+            results = pool.map(train_model_parallel, args)
+
+        for model_name, num_epochs, learning_rate, weight_decay, acc_media, rep_max, duracao in results:
+            print(f"Parâmetro de modelo: {model_name}")
+            print(f"Parâmetro de quantidade de épocas: {num_epochs}")
+            print(f"Parâmetro de Learning Rate: {learning_rate}")
+            print(f"Parâmetro de Weight Decay: {weight_decay}")
+            print(f"Acurácia Média: {acc_media}")
+            print(f"Melhor replicação: {rep_max}")
+            print(f"Tempo: {duracao:.2f} segundos\n")
+            print("-----------------------------------")
+        fim_sistema = time.time()
+        print(f"Tempo total para o sistema Centralizado Multiprocesso: {fim_sistema - inicio_sistema:.2f} segundos\n")
+
+    elif escolha == "3":
+        inicio_sistema = time.time()  # Início do sistema distribuído multiprocesso
+        print("Sistema Distribuído em Multiprocesso Escolhido.")
+        # Lógica futura para processamento distribuído
+        fim_sistema = time.time()
+        print(f"Tempo total para o sistema Distribuído Multiprocesso: {fim_sistema - inicio_sistema:.2f} segundos\n")
+    
+    else:
+        print("Escolha inválida. Execute o programa novamente e selecione uma opção válida.")
+
+    fim_total = time.time()
+    print(f"Tempo total de execução do programa: {fim_total - inicio_total:.2f} segundos")
